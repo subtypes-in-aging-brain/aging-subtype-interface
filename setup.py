5f1ab98f05e9d0595dfd86f631a2ac68805cf46b
vikram <88.vikram@gmail.com>
2021-07-16 13:04:33 +0000

    Basic structure of the repository

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="aging-subtype-interface",
+    version="0.1",
+    author="Vikram Venkatraghavan",
+    author_email="v.venkatraghavan@erasmusmc.nl",
+    description="Backend functions for the webapplication",
+    long_description="Same as above",
+    long_description_content_type="text/markdown",
+    url="https://github.com/subtypes-in-aging-brain/aging-subtype-interface",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+      'numpy',
+      'pandas',
+      'scikit-learn',
+      'scipy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
